5a6f79b3ccbbb9a0f933939ba575d684b404db33
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-11-11 21:46:51 -0300

    Criação de componente do Video

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,12 @@
+export const Hero: React.FC<any> = () => {
+  let videoUrl: string =
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+  return (
+    <div>
+      <video className="videoPlayer" controls autoPlay>
+        <source src={videoUrl} type="video/mp4" />
+        Your browser does not support HTML video.
+      </video>
+    </div>
+  );
+};
