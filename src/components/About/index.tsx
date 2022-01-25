30c239371d2121ef702bb843c539b53f35a9d91a
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-01-24 22:35:32 -0300

    About - texto

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -18,11 +18,12 @@ export const About: React.FC<any> = () => {
           less than 2 hours of music, with a essential rule: nothing pre-2020,
           only new music, to support artists who have been struggling since the
           beginning of the pandemic to promote their new work without touring.
+        </p>
+
+        <p className="aboutText">
           Here you can listen to all the playlists shared by Laura, who will
           make sure you find your new favorite song!
         </p>
-
-        <p className="aboutText">Lorem!!</p>
       </div>
 
       <div className="imageContainer">
