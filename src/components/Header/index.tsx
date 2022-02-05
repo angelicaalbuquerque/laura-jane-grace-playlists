5daac0ea7879af60bb0853a7147df312fc7a8662
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-02-05 18:05:12 -0300

    NextDJSet bar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,33 @@
+import { Fragment } from "react";
 import logo from "../../assets/ynfs-logo.svg";
-import { HeaderContainer, HeaderContent } from "./style";
+import {
+  HeaderContainer,
+  HeaderContent,
+  ButtonVans,
+  NextSetBar,
+} from "./style";
 
 export const Header: React.FC<any> = () => {
   return (
-    <HeaderContainer>
-      <HeaderContent>
-        <img src={logo} alt="your new favorite song with Laura Jane Grace" />
-        <nav>
-          <ul className="linksContainer">
-            <li>Playlists</li>
-            <li>About</li>
-            <li>6 Questions</li>
-            <li>
-              <button className="buttonCTA">Channel 66</button>
-            </li>
-          </ul>
-        </nav>
-      </HeaderContent>
-    </HeaderContainer>
+    <Fragment>
+      <HeaderContainer>
+        <HeaderContent>
+          <img src={logo} alt="your new favorite song with Laura Jane Grace" />
+          <nav>
+            <ul className="linksContainer">
+              <li>Playlists</li>
+              <li>About</li>
+              <li>6 Questions</li>
+              <li>
+                <ButtonVans>Channel 66</ButtonVans>
+              </li>
+            </ul>
+          </nav>
+        </HeaderContent>
+      </HeaderContainer>
+      <NextSetBar>
+        <p>next DJ set: Feb 24th, 12pm EST</p>
+      </NextSetBar>
+    </Fragment>
   );
 };
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -20,6 +20,12 @@ export const HeaderContent = styled.div`
   max-width: 1200px;
   padding: 0 20px;
 
+  .linksContainer {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+  }
+
   @media (max-width: 800px) {
     display: flex;
     flex-direction: column;
@@ -30,4 +36,29 @@ export const HeaderContent = styled.div`
       margin-top: 20px;
     }
   }
+`;
+
+export const ButtonVans = styled.button`
+  padding: 0.5rem;
+  font-size: 1rem;
+  font-weight: bold;
+  color: #1e2b25;
+  background-color: #35ac79;
+  border-radius: 4px;
+  border: none;
+  &:hover {
+    background-color: #ffffff;
+  }
+`;
+
+export const NextSetBar = styled.div`
+  background-color: #242426;
+  border-top: 0.5px solid #363636;
+  border-bottom: 0.5px solid #363636;
+  color: white;
+  display: flex;
+  justify-content: center;
+  font-size: 0.875rem;
+  font-weight: bold;
+  padding: 5px 0;
 `;
