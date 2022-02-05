4afeabf0bc2aa46c5abb10d716493436e9a3c6cd
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-02-05 16:14:03 -0300

    Lógica de carregar botão e 4 primeiras playlists

--- src/components/Playlists/index.tsx
+++ src/components/Playlists/index.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import playlistsIcon from "./../../assets/playlists.svg";
 import {
   SectionPlaylistContainer,
@@ -8,7 +10,21 @@ import {
 import albumInfo from "./data";
 
 export const Playlists: React.FC<any> = () => {
-  let albumData = albumInfo;
+  let [loadMore2021, setLoadMore2021] = useState(false);
+
+  const [album2021, setAlbum2021] = useState(
+    albumInfo.filter((x, index) => (loadMore2021 ? x : index < 4)),
+  );
+
+  let album2022 = albumInfo.filter((x) => x.albumYear === "2022");
+
+  let loadMoreBtn = () => {
+    setLoadMore2021(true);
+
+    let filtroAlbum2021 = albumInfo.filter((x) => x.albumYear === "2021");
+
+    setAlbum2021(filtroAlbum2021);
+  };
 
   return (
     <SectionPlaylistContainer>
@@ -21,46 +37,46 @@ export const Playlists: React.FC<any> = () => {
         <CardPlaylistContainer>
           <h3 className="playlistSubtitle">2021</h3>
           <PlaylistItems>
-            {albumData
-              .filter((x) => x.albumYear === "2021")
-              .map((albumCover, index) => {
-                return (
-                  <div key={index} className="playlistCard">
-                    <a href={albumCover.albumUrl} target="_blank">
-                      <img
-                        src={albumCover.imgUrl}
-                        alt={`Album art: ${albumCover.albumMonth}_${albumCover.albumYear}`}
-                      />
-                      <span className="playlistHoverYear">
-                        {albumCover.albumMonth}
-                      </span>
-                    </a>
-                  </div>
-                );
-              })}
+            {album2021.map((albumCover, index) => {
+              return (
+                <div key={index} className="playlistCard">
+                  <a href={albumCover.albumUrl} target="_blank">
+                    <img
+                      src={albumCover.imgUrl}
+                      alt={`Album art: ${albumCover.albumMonth}_${albumCover.albumYear}`}
+                    />
+                    <span className="playlistHoverYear">
+                      {albumCover.albumMonth}
+                    </span>
+                  </a>
+                </div>
+              );
+            })}
           </PlaylistItems>
+
+          {loadMore2021 === false && (
+            <button onClick={() => loadMoreBtn()}>load more</button>
+          )}
         </CardPlaylistContainer>
 
         <CardPlaylistContainer>
           <h3 className="playlistSubtitle">2022</h3>
           <PlaylistItems>
-            {albumData
-              .filter((x) => x.albumYear === "2022")
-              .map((albumCover, index) => {
-                return (
-                  <div key={index} className="playlistCard">
-                    <a href={albumCover.albumUrl} target="_blank">
-                      <img
-                        src={albumCover.imgUrl}
-                        alt={`Album art: ${albumCover.albumMonth}_${albumCover.albumYear}`}
-                      />
-                      <span className="playlistHoverYear">
-                        {albumCover.albumMonth}
-                      </span>
-                    </a>
-                  </div>
-                );
-              })}
+            {album2022.map((albumCover, index) => {
+              return (
+                <div key={index} className="playlistCard">
+                  <a href={albumCover.albumUrl} target="_blank">
+                    <img
+                      src={albumCover.imgUrl}
+                      alt={`Album art: ${albumCover.albumMonth}_${albumCover.albumYear}`}
+                    />
+                    <span className="playlistHoverYear">
+                      {albumCover.albumMonth}
+                    </span>
+                  </a>
+                </div>
+              );
+            })}
           </PlaylistItems>
         </CardPlaylistContainer>
       </div>
