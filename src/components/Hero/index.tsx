c611f8f362d5cbc8e60f6b6cdfbb5511afeb2fd9
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-02-05 22:34:14 -0300

    video hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,10 +3,20 @@ export const Hero: React.FC<any> = () => {
     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
   return (
     <div>
-      {/* <video className="videoPlayer" controls autoPlay>
-        <source src={videoUrl} type="video/mp4" />
-        Your browser does not support HTML video.
-      </video> */}
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/legbLKCJ6hU?controls=0"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+
+      <p>
+        Stream all the playlists made by Laura Jane Grace as resident DJ for
+        VANS' Channel 66 and find your new favorite song!
+      </p>
     </div>
   );
 };
